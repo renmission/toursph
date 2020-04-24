ead242dfeff799e7fdd4415785006fb871473f06
gitMission <missionrenjr@gmail.com>
2020-04-24 19:50:19 +0800

    Implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 
 const AppError = require('./utils/appError');
@@ -26,6 +27,10 @@ app.enable('trust proxy');
 // template engine
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(cors());
+
+app.options('*', cors());
 
 // serving static files
 app.use(express.static(path.join(__dirname, 'public')));
